7b5b1d0aba983d351cf171043e76683d9050ff5e
avivashishta <78400794+avivashishta@users.noreply.github.com>
2021-08-15 13:01:09 -0400

    final commit

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -96,12 +96,15 @@ register ={
 variables = dict()
 labels = []
 
-def assembler(line,count,finalcount,flag1,flag2,temp):
+def assembler(line,count,finalcount,flag1,flag2):
    
-    with open('stdout.txt', 'a') as f:
-        temp = False
+    with open('Simple-Assembler/stdout.txt', 'a') as f:
 
         input_arr = line.split()
+
+        if finalcount==count and input_arr[0]!="hlt":
+            f.writelines("missing hlt instruction "+"at line "+str(count+1)+"\n")
+            exit()
 
         if input_arr[0]=="hlt":
             temp = True
@@ -115,16 +118,19 @@ def assembler(line,count,finalcount,flag1,flag2,temp):
 
         if input_arr[0] not in instructions and input_arr[0][:-1]==":":
             if input_arr[0][0:-2] in variables:
-                print ("Misuse of label as a variable"+"at line"+str(count))
+                f.writelines ("Misuse of label as a variable "+"at line "+str(count)+"\n")
+                exit()
             else:
                 input_arr[0] = input_arr[0][1:]
                 labels.append(input_arr[0])  
 
         if "FLAGS" in input_arr and not input_arr[0]=="mov":
-            print("Illegal use of FLAGS Register"+"at line"+str(count))
+            f.writelines("Illegal use of FLAGS Register "+"at line "+str(count)+"\n")
+            exit()
         
         if input_arr[0]=="var" and flag2 ==True:
-            print("Variables not declared at the beginning"+"at line"+str(count))
+            f.writelines("Variables not declared at the beginning "+"at line "+str(count)+"\n")
+            exit()
 
         if input_arr[0] in instructions:
 
@@ -134,63 +140,82 @@ def assembler(line,count,finalcount,flag1,flag2,temp):
 
             if instructions[input_arr[0]]["type"] == "A":
                 if len(input_arr)!=4:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
                 if input_arr[1] not in register or input_arr[2] not in register or input_arr[3] not in register:
-                    print("Typo in Register Name"+"at line"+str(count))
+                    f.writelines("Typo in Register Name "+"at line"+str(count)+"\n")
+                    exit()
             
-                print(instructions[input_arr[0]]["opcode"]+"00"+register[input_arr[1]]+register[input_arr[2]]+register[input_arr[3]])
+                f.writelines(instructions[input_arr[0]]["opcode"]+"00"+register[input_arr[1]]+register[input_arr[2]]+register[input_arr[3]]+"\n")
             
             elif instructions[input_arr[0]]["type"] == "B":
                 if len(input_arr)!=3:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
 
                 if input_arr[1] not in register:
-                    print("Typo in Register Name"+"at line"+str(count))
+                    f.writelines("Typo in Register Name " +"at line "+str(count)+"\n")
+                    exit()
 
                 if int(input_arr[2][1:])<0 or int(input_arr[2][1:])>255:
-                    print("Illegal Immediate Value"+"at line"+str(count))
+                    f.writelines("Illegal Immediate Value "+"at line "+str(count)+"\n")
+                    exit()
 
-                print(instructions[input_arr[0]]["opcode"]+register[input_arr[1]]+'{0:08b}'.format(int(input_arr[2][1:])))
+                f.writelines(instructions[input_arr[0]]["opcode"]+register[input_arr[1]]+'{0:08b}'.format(int(input_arr[2][1:]))+"\n")
             
             elif instructions[input_arr[0]]["type"] == "C":
                 if len(input_arr)!=3:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
                 if input_arr[1] or input_arr[2] not in register:
-                    print("Typo in Register Name"+"at line"+str(count))
-                print(instructions[input_arr[0]]["opcode"]+"00000"+register[input_arr[1]]+register[input_arr[2]])
+                    f.writelines("Typo in Register Name "+"at line"+str(count)+"\n")
+                    exit()
+
+                f.writelines(instructions[input_arr[0]]["opcode"]+"00000"+register[input_arr[1]]+register[input_arr[2]]+"\n")
             elif instructions[input_arr[0]]["type"] == "D":
                 if len(input_arr)!=3:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
                 if input_arr[2] not in variables:
-                    print("Use of Undefined Variable"+"at line"+count)
+                    f.writelines("Use of Undefined Variable "+"at line "+count+"\n")
+                    exit()
                 if input_arr[1] not in register:
-                    print("Typo in Register Name"+"at line"+count)
-                print(instructions[input_arr[0]]["opcode"]+register[input_arr[1]]+'{0:08b}'.format((finalcount-len(variables))+variables[input_arr[2]]))
+                    f.writelines("Typo in Register Name "+"at line "+count+"\n")
+                    exit()
+                f.writelines(instructions[input_arr[0]]["opcode"]+register[input_arr[1]]+'{0:08b}'.format((finalcount-len(variables))+variables[input_arr[2]])+"\n")
             elif instructions[input_arr[0]]["type"] == "E":
                 if len(input_arr)!=2:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
                 if input_arr[2] not in variables:
-                    print("Use of Undefined Variable"+"at line"+str(count))
+                    f.writelines("Use of Undefined Variable "+"at line "+str(count)+"\n")
+                    exit()
                 if input_arr[2] not in labels:
-                    print("Use of Undefined Labels"+"at line"+str(count))
-                print(instructions[input_arr[0]]["opcode"]+"000"+input_arr[1])
+                    f.writelines("Use of Undefined Labels "+"at line "+str(count)+"\n")
+                    exit()
+                f.writelines(instructions[input_arr[0]]["opcode"]+"000"+input_arr[1]+"\n")
 
             elif instructions[input_arr[0]]["type"] == "F":
                 
                 if len(input_arr)!=1:
-                    print("Wrong Syntax for this type of instruction"+"at line"+str(count))
+                    f.writelines("Wrong Syntax for this type of instruction "+"at line "+str(count)+"\n")
+                    exit()
 
                 if count != finalcount:
-                    print("hlt not being used as last instruction"+"at line"+str(count))
+                    f.writelines("hlt not being used as last instruction "+"at line "+str(count)+"\n")
+                    exit()
 
-                print(instructions[input_arr[0]]["opcode"]+"0"*11)
+                f.writelines(instructions[input_arr[0]]["opcode"]+"0"*11+"\n")
+                
             
             else:
-                print("Typos in Instruction name"+"at line"+str(count))
+                f.writelines("Typos in Instruction name "+"at line "+str(count)+"\n")
+                exit()
         
         
         if input_arr[0] not in instructions and input_arr[0] != "var" and input_arr[0][:-1]!=":" :
-            print("General Syntax Error"+str(count))
+            f.writelines("General Syntax Error "+"at line "+str(count)+"\n")
+            exit()
 
             
         
@@ -204,8 +229,6 @@ def main():
     with open('Simple-Assembler/stdin.txt' , 'r') as f1:
         for line in f1:
             finalcount += 1
-
-    temp = False
     flag1 = False
     flag2 = False    
     with open('Simple-Assembler/stdin.txt', 'r') as f1:
@@ -213,9 +236,7 @@ def main():
             if line == '': # If empty string is read then stop the loop
                 break
             count+=1
-            assembler(line,count,finalcount,flag1,flag2,temp) # perform some operation(s) on given string
-        if temp == False:
-                print("Missing hlt instruction")
+            assembler(line,count,finalcount,flag1,flag2) # perform some operation(s) on given string
 
 if __name__ == '__main__':
 	main()
